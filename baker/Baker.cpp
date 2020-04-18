48d0883b5c7222d2bb68bbf65262532a7fcc69ce
Dylan Wintermute <dylan.wintermute@cnu.edu>
2020-04-18 00:45:14 -0400

    baker is should be done.

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -1,6 +1,8 @@
 #include <mutex>
 
 #include "../includes/baker.h"
+#include "../includes/externs.h"
+#include "../includes/constants.h"
 using namespace std;
 
 Baker::Baker(int id):id(id)
@@ -12,9 +14,52 @@ Baker::~Baker()
 }
 
 void Baker::bake_and_box(ORDER &anOrder) {
+	int donutCount = anOrder.number_donuts;
 	
+	Box b;
+	DONUT d;
+	while (donutCount != 0) {
+
+		if (b.addDonut(d) == false){
+			anOrder.boxes.push_back(b);
+			b.clear();
+		}
+		donutCount--;
+	}
+
+
+
+	order_out_Vector.push_back(anOrder);
+
+
 }
 
 void Baker::beBaker() {
 	
+	while (true){
+		unique_lock<mutex> lck(mutex_order_inQ);
+		while(order_in_Q.size() == 0 && b_WaiterIsFinished == false){
+			cv_order_inQ.wait(lck);
+		}
+
+		if (order_in_Q.size() > 0){
+				bake_and_box(order_in_Q.front());
+				order_in_Q.pop();
+
+				lck.unlock();
+				cv_order_inQ.notify_all();
+		}
+		if (b_WaiterIsFinished){
+			while (order_in_Q.size() != 0){
+				bake_and_box(order_in_Q.front());
+				order_in_Q.pop();
+
+			}
+			break;
+		}
+
+
+
+	}
+
 }
